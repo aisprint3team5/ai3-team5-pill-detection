3d7dc61a9e21afac9f0cced2b1b1294e5a678e53
dev-carki <94.nogari@kakao.com>
2025-07-23 17:46:10 +0900

    [refactor] dir code refactored

--- models/yolo8/detector/YOLOv8_detector.py
+++ models/yolo8/detector/YOLOv8_detector.py
@@ -9,6 +9,7 @@ class YOLOV8Detector():
     def __init__(self, model_path: str, conf_threshold: float):
         self.model = YOLO(model_path)
         self.conf_threshold = conf_threshold
+        self.run_time = f"{datetime.datetime.now().strftime('%y%m%d_%H-%M-%S')}"
         
         print(f"[DEV - INFO] Loaded model: {model_path}")
         print(f"[DEV - INFO] Classes: {self.model.names}")
@@ -16,7 +17,6 @@ class YOLOV8Detector():
     def train(self, data_yaml_path: str, epochs: int = 10, patience: int = 5, imgsz: int = 416, batch_size: int = 16):
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print(f"[DEV - INFO] Training on {data_yaml_path}, device: {device}")
-        run_name = f"train_results_{datetime.datetime.now().strftime('%y%m%d_%H-%M-%S')}"
         
         self.model.train(
             data=data_yaml_path,
@@ -25,7 +25,7 @@ class YOLOV8Detector():
             imgsz=imgsz,
             batch=batch_size,
             project="runs/detect/train",
-            name=run_name,
+            name=f"train_results_{self.run_time}",
             device=device,
             exist_ok=True
         )
