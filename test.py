22eb4593ea99b64e17d950a3c6db962bdda5cab2
candy0543 <candy0543@naver.com>
2025-07-24 02:04:44 +0100

    add path var in data.yaml & modify test.py

--- test.py
+++ test.py
@@ -45,81 +45,87 @@ VAL_SPLIT = 0.1
 
 # if not is_data_downloaded(CONFIG_ORIGINAL_DATASET):
 #     print("Data is not downloaded. Downloading...")
-download_data(CONFIG_ORIGINAL_DATASET)
+#download_data(CONFIG_ORIGINAL_DATASET)
 # else:
 #     print("Data is already downloaded.")
 
-
-dataset = convert_to_json(CONFIG_DB_PATH)
-
-
-
- # Parse json file
-parser = dataset_parser.PillDatasetParser(CONFIG_TRAIN_IMAGE_DIR, CONFIG_TRAIN_ANNO_DIR)
-if os.path.exists(CONFIG_CACHE_DIR):
-    dataset = parser.load_from_pickle(CONFIG_CACHE_DIR)
-else:
-    # Parse and save
-    dataset = parser.parse()
-    parser.save_to_pickle(CONFIG_CACHE_DIR)
+def main():
+    print("Running test script...")
+    dataset = convert_to_json(CONFIG_DB_PATH)
 
 
-all_class_names = set()
-for data in dataset:
-    for cat in data["categories"]:
-        all_class_names.add(cat["category_name"])
-class_names = sorted(list(all_class_names))
-class_to_idx = {name: idx for idx, name in enumerate(class_names)}
 
-# #  디렉토리 생성
-for split in ["train", "val"]:
-    for t in ["images", "labels"]:
-        os.makedirs(os.path.join(CONFIG_SPLIT_DATA_DIR, split, t), exist_ok=True)
+    # Parse json file
+    parser = dataset_parser.PillDatasetParser(CONFIG_TRAIN_IMAGE_DIR, CONFIG_TRAIN_ANNO_DIR)
+    if os.path.exists(CONFIG_CACHE_DIR):
+        dataset = parser.load_from_pickle(CONFIG_CACHE_DIR)
+    else:
+        # Parse and save
+        dataset = parser.parse()
+        parser.save_to_pickle(CONFIG_CACHE_DIR)
 
-# # split dataset
-random.shuffle(dataset)
-val_size = int(len(dataset) * VAL_SPLIT)
-val_data = dataset[:val_size]
-train_data = dataset[val_size:]
 
-convert_to_yolo(train_data, class_to_idx, CONFIG_TRAIN_IMAGE_DIR, CONFIG_TRAIN, CONFIG_TRAIN_LABEL)
-convert_to_yolo(val_data,class_to_idx, CONFIG_TRAIN_IMAGE_DIR, CONFIG_VAL, CONFIG_VAL_LABEL)
-# # #
-# split after dataset is converted to YOLO format 
-# split_yolo_dataset( CONFIG_INPUT_IMAGE_DIR,
-#             CONFIG_INPUT_LABEL_DIR,
-#             CONFIG_OUTPUT_DIR)
+    all_class_names = set()
+    for data in dataset:
+        for cat in data["categories"]:
+            all_class_names.add(cat["category_name"])
+    class_names = sorted(list(all_class_names))
+    class_to_idx = {name: idx for idx, name in enumerate(class_names)}
 
-data_yaml = {
-    "yolo_train_dir": CONFIG_TRAIN,
-    "yolo_train_label": CONFIG_TRAIN_LABEL,
-    "yolo_val_dir": CONFIG_VAL,
-    "yolo_val_label": CONFIG_VAL_LABEL,
-    "train": CONFIG_TRAIN,
-    "val": CONFIG_VAL,
-    "nc": len(class_names),
-    "names": class_names
-}
-#Save data.yaml
-with open(CONFIG_DATA_YAML_PATH, "w") as f:
-    yaml.dump(data_yaml, f, allow_unicode=True)
+    # #  디렉토리 생성
+    for split in ["train", "val"]:
+        for t in ["images", "labels"]:
+            os.makedirs(os.path.join(CONFIG_SPLIT_DATA_DIR, split, t), exist_ok=True)
 
-class_name_to_idx, _ = load_class_mapping(CONFIG_CLASS_FILE)
+    # # split dataset
+    random.shuffle(dataset)
+    val_size = int(len(dataset) * VAL_SPLIT)
+    val_data = dataset[:val_size]
+    train_data = dataset[val_size:]
 
-# Define transformation
-transform = PillImageTransform(resize=(640, 640))
+    convert_to_yolo(train_data, class_to_idx, CONFIG_TRAIN_IMAGE_DIR, CONFIG_TRAIN, CONFIG_TRAIN_LABEL)
+    convert_to_yolo(val_data,class_to_idx, CONFIG_TRAIN_IMAGE_DIR, CONFIG_VAL, CONFIG_VAL_LABEL)
+    # # #
+    # split after dataset is converted to YOLO format 
+    # split_yolo_dataset( CONFIG_INPUT_IMAGE_DIR,
+    #             CONFIG_INPUT_LABEL_DIR,
+    #             CONFIG_OUTPUT_DIR)
 
-# # Load train and val datasets
-train_dataset = PillYoloDataset(
-    image_dir=CONFIG_TRAIN,
-    label_dir=CONFIG_TRAIN_LABEL,
-    class_to_idx=class_name_to_idx,
-    S=7, B=2, C=len(class_name_to_idx),
-    transform=transform
-)
-val_dataset = PillYoloDataset(
-    image_dir=CONFIG_VAL,
-    label_dir=CONFIG_VAL_LABEL,
-    class_to_idx=class_name_to_idx,
-    transform=transform
-)
+    data_yaml = {
+        "yolo_train_dir": CONFIG_TRAIN,
+        "yolo_train_label": CONFIG_TRAIN_LABEL,
+        "yolo_val_dir": CONFIG_VAL,
+        "yolo_val_label": CONFIG_VAL_LABEL,
+        "train": CONFIG_TRAIN,
+        "val": CONFIG_VAL,
+        "path": CONFIG_SPLIT_DATA_DIR,
+        "nc": len(class_names),
+        "names": class_names
+    }
+    #Save data.yaml
+    with open(CONFIG_DATA_YAML_PATH, "w") as f:
+        yaml.dump(data_yaml, f, allow_unicode=True)
+
+    class_name_to_idx, _ = load_class_mapping(CONFIG_CLASS_FILE)
+
+    # Define transformation
+    transform = PillImageTransform(resize=(640, 640))
+
+    # # Load train and val datasets
+    train_dataset = PillYoloDataset(
+        image_dir=CONFIG_TRAIN,
+        label_dir=CONFIG_TRAIN_LABEL,
+        class_to_idx=class_name_to_idx,
+        S=7, B=2, C=len(class_name_to_idx),
+        transform=transform
+    )
+    val_dataset = PillYoloDataset(
+        image_dir=CONFIG_VAL,
+        label_dir=CONFIG_VAL_LABEL,
+        class_to_idx=class_name_to_idx,
+        transform=transform
+    )
+    print("Finished test script...")
+
+if __name__ == "__main__":
+    main()
