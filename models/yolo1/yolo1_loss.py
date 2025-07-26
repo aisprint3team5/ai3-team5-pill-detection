7e4acaed58d7067ddbdb8362d232af36f8688ac0
AI Questor <AI Questor>
2025-07-26 09:18:47 +0900

    minor

--- models/yolo1/yolo1_loss.py
+++ models/yolo1/yolo1_loss.py
@@ -139,7 +139,7 @@ def Yolo1Loss(S, B, C, lambda_coord=5.0, lambda_noobj=0.5):
         batch_size = predictions.size(0)
         # return total_loss / batch_size  # 배치 사이즈로 나눠준다.
 
-        if total_loss is None:
+        if torch.isnan(total_loss):
             print(f'box_loss={box_loss.item():.6f}, obj_conf={obj_conf_loss.item():.6f}, noobj={noobj_loss.item():.6f}, class={class_loss.item():.6f}')
             print('total before /batch', total_loss)
             print('batch_size:', batch_size)
